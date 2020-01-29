18e8f5dfd58dc7c880f995d8a6115747faa18810
waqar-ucf <54361378+waqar-ucf@users.noreply.github.com>
2020-01-29 19:50:24 +0500

    issue try 6

--- app/src/com/ucf/salesforcenative/MainActivity.java
+++ app/src/com/ucf/salesforcenative/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends SalesforceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		//adding new commit to old commit
 		// Setup theme
 		boolean isDarkTheme = MobileSyncSDKManager.getInstance().isDarkTheme();
 		setTheme(isDarkTheme ? R.style.SalesforceSDK_Dark : R.style.SalesforceSDK);
